d49ab699848003dce90b43b6a3dea7598f5d1460
Russell Gordon <rgordon@lcs.on.ca>
2020-11-04 12:15:06 -0500

    Initial Commit

---
+++ PasswordlessSwiftUIApp/PasswordlessSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordlessSwiftUIAppApp.swift
+//  PasswordlessSwiftUIApp
+//
+//  Created by Russell Gordon on 2020-11-04.
+//
+
+import SwiftUI
+
+@main
+struct PasswordlessSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
