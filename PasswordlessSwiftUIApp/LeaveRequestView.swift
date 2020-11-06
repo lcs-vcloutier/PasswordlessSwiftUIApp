6c55807d94e8d4c99f3c5bc4bc7e56e7aa511b7f
Vincent Cloutier <vincentxcloutier@gmail.com>
2020-11-06 12:08:54 -0500

    leave request view working ui , need to solve backend firebase logic

--- PasswordlessSwiftUIApp/LeaveRequestView.swift
+++ PasswordlessSwiftUIApp/LeaveRequestView.swift
@@ -9,30 +9,30 @@ import SwiftUI
 
 struct LeaveRequestView: View {
     let typesOfLeave = ["Lakefield", "Bridge", "Peterborough"]
-        @State private var typeOfLeave : Int = 0
-        @State private var numberOfPeople: Int = 0
-        @State private var time: Int = 0
+    @State private var typeOfLeave : Int = 0
+    @State private var numberOfPeople: Int = 0
+    @State private var time: Int = 0
     
     var body: some View {
         VStack(alignment: .leading) {
-    
+            
             
             Form {
-                        Section(header: Text("Type of Leave")) {
-                            Picker("Type Of Leave", selection: $typeOfLeave) {
-                                Text("Lakefield").tag(0)
-                                Text("Bridge").tag(1)
-                                Text("Peterborough").tag(2)
-                            }
-                            .pickerStyle(SegmentedPickerStyle())
-                        }
-        
-            CustomStyledButton(title: "Go to \(typesOfLeave[typeOfLeave])", action: {
-                // set time of leave do database logic etc. & go to during leave view
+                Section(header: Text("Type of Leave")) {
+                    Picker("Type Of Leave", selection: $typeOfLeave) {
+                        Text("Lakefield").tag(0)
+                        Text("Bridge").tag(1)
+                        Text("Peterborough").tag(2)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
                 
-            })    // 2
-               
-        }
+                CustomStyledButton(title: "Go to \(typesOfLeave[typeOfLeave])", action: {
+                    // set time of leave do database logic etc. & go to during leave view
+                    
+                })   
+                
+            }
         }
         .navigationBarTitle("Leave Request")
     }
