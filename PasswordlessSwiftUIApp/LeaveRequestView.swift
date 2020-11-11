4862d3ab70ba5473e374f83a448dd5b9ec33b6c2
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-11 10:39:40 -0500

    cleaned up LeaveRequest view

--- PasswordlessSwiftUIApp/LeaveRequestView.swift
+++ PasswordlessSwiftUIApp/LeaveRequestView.swift
@@ -10,30 +10,25 @@ import SwiftUI
 struct LeaveRequestView: View {
     let typesOfLeave = ["Lakefield", "Bridge", "Peterborough"]
     @State private var typeOfLeave : Int = 0
-    @State private var numberOfPeople: Int = 0
     @State private var time: Int = 0
     
     var body: some View {
         VStack(alignment: .leading) {
-            
-            
-            Form {
-                Section(header: Text("Type of Leave")) {
                     Picker("Type Of Leave", selection: $typeOfLeave) {
                         Text("Lakefield").tag(0)
                         Text("Bridge").tag(1)
                         Text("Peterborough").tag(2)
                     }
                     .pickerStyle(SegmentedPickerStyle())
-                }
+                                 
                 
                 CustomStyledButton(title: "Go to \(typesOfLeave[typeOfLeave])", action: {
                     // set time of leave do database logic etc. & go to during leave view
                     
-                })   
-                
-            }
+                })
+            Spacer()
         }
+        .padding()
         .navigationBarTitle("Leave Request")
     }
 }
