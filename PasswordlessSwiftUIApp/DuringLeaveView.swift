dbc059eef28a16e1b0db6de35b4f435f4d3898fd
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-11 10:33:58 -0500

    added during leave view, still struggling with the database & userDefaults

---
+++ PasswordlessSwiftUIApp/DuringLeaveView.swift
@@ -0,0 +1,20 @@
+//
+//  DuringLeaveView.swift
+//  PasswordlessSwiftUIApp
+//
+//  Created by Cloutier, Vincent on 2020-11-11.
+//
+
+import SwiftUI
+
+struct DuringLeaveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DuringLeaveView_Previews: PreviewProvider {
+    static var previews: some View {
+        DuringLeaveView()
+    }
+}
